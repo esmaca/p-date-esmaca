f0da32384fb1388ea9b77560ea078ad80b88b408
esmaca <ematec00@estudiantes.unileon.es>
2019-10-31 11:44:14 +0100

    Initial commit

---
+++ src/es/unileon/prg1/pdate/Date.java
@@ -0,0 +1,5 @@
+package es.unileon.prg1.pdate;
+
+public class Date {
+
+}
---
+++ src/es/unileon/prg1/pdate/PDatePrincipal.java
@@ -0,0 +1,10 @@
+package es.unileon.prg1.pdate;
+
+public class PDatePrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
