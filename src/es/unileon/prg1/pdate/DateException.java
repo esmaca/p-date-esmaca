2d6bac9fa8e9a2427af9141661e4d61f8b820698
esmaca <ematec00@estudiantes.unileon.es>
2019-11-04 12:31:21 +0100

    Olvidado en el anterior comit

---
+++ src/es/unileon/prg1/pdate/DateException.java
@@ -0,0 +1,12 @@
+package es.unileon.prg1.pdate;
+
+public class DateException extends Exception {
+	
+	
+	//private static final version
+	
+	public DateException(String msg){
+		super(msg);
+	}
+
+}
